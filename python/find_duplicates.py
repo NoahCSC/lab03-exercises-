fcc9e8bd257f1fe62c46de7e3d2923df8a299bf1
NoahCSC <NoahCSC@gmail.com>
2025-09-05 17:42:59 -0400

    Changed python code to include set function.

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -7,7 +7,16 @@ def find_duplicates_nested_loop(l: list) -> list:
                 if l[i] not in result:
                     result.append(l[i])
     return result
-       
+def find_duplicates_set(l:list)->list:
+    s1=set()
+    result=[]
+    for i in range(len(l)):
+        if l[i] in s1:
+            if l[i] not in result:
+                result.append(l[i])
+        else:
+            s1.add(l[i])
+    return result       
     
 if __name__ == "__main__":
     sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
@@ -19,3 +28,7 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+    print("Sample 5:", find_duplicates_set(sample1))
+    print("Sample 6:", find_duplicates_set(sample2))
+    print("Sample 7:", find_duplicates_set(sample3))
+    print("Sample 8:", find_duplicates_set(sample4))
